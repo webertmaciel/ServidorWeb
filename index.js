e7078c8713142013112ea9baf67f5d028ec2622e
webert maciel <webert_maciel@hotmail.com>
2021-11-02 17:27:25 -0300

    first modification

---
+++ index.js
@@ -0,0 +1,59 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+
+
+function handleFile(req, res, callback) {
+    let path = url.parse(req.url).pathname;
+
+    let fileName = "." + path;
+
+
+    fs.readFile(fileName, (err, data) => {
+        if (err) {
+
+            if (callback) {
+                if (!callback(req, res)) {
+                    res.writeHead(404, { "content-type": "text/html;charset=utf-8" });
+                    res.end("<h1>Pagina nao encontrada</h1>")
+                }
+            }
+
+
+
+
+
+        } else {
+            res.writeHead(200, { "content-type": "text/html" })
+            res.write(data);
+            res.end();
+        }
+    })
+
+}
+
+function handleRequest(req, res) {
+
+    let path = url.parse(req.url).pathname;
+
+
+    if (path == "/teste") {
+        res.end("teste");
+        return true;
+    }
+    return false;
+}
+
+
+
+
+http.createServer((request, response) => {
+    handleFile(request, response, handleRequest);
+}).listen(3000, err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Servidor Rodando na Porta 3000")
+    }
+})
